602b48e13ddad846d65ff2c4fa790764af44cd8e
Don Smith <don@antiantonym.com>
2020-03-02 18:47:12 +1300

    PreciseTime :arrow_right: Instant

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,21 @@ mod block;
 
 use block::factory;
 use block::is_valid;
-use time::PreciseTime;
+use time::Instant;
 
 fn main() {
     println!("Creating Genesis block");
-    let mut start = PreciseTime::now();
+    let genesis_instant = Instant::now();
     let genesis_block = factory::make_genesis();
-    let mut end = PreciseTime::now();
     println!("Genesis:  {:?}", genesis_block);
-    println!("Genesis creation time: {}", start.to(end));
+    println!("Genesis creation time: {:?}", genesis_instant.elapsed());
     println!("Genesis block is valid: {}", is_valid(&genesis_block));
 
     println!("Creating Block #2");
-    start = PreciseTime::now();
+    let block_instant = Instant::now();
     let block_2_data = String::from("Data for block #2");
     let block_2 = factory::make_regular(&genesis_block, block_2_data);
-    end = PreciseTime::now();
     println!("Block #2: {:?}", block_2);
-    println!("Block #2 creation time: {}", start.to(end));
+    println!("Block #2 creation time: {:?}", block_instant.elapsed());
     println!("Block #2 is valid: {}", is_valid(&block_2));
 }
